ab110bcba883bc0016480c38517b50c9b633e471
이정철 <ghop0822@gmail.com>
2025-08-25 00:03:49 +0900

    Add files via upload

--- main.dart
+++ main.dart
@@ -6,10 +6,57 @@ void main() {
 
 class MyApp extends StatelessWidget{
   const MyApp({Key? key}) : super(key: key);
-@override
-Widget build(BuildContext context) {
-  return MaterialApp(
-      home: Icon(Icons.star)
-  );
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Color(0xFF4287F8),
+          ),
+          body:
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ListView(
+                    children: [
+                         Row(
+                          children: [
+                            Icon(Icons.account_circle,size:70),
+                            SizedBox(width: 5,),
+                            Text('홍길동',style: TextStyle(fontSize: 24.0),)
+                          ],
+                        ),
+                      SizedBox(height: 10,),
+                      Row(
+                        children: [
+                          Icon(Icons.account_circle,size:70),
+                          SizedBox(width: 5,),
+                          Text('홍길동',style: TextStyle(fontSize: 24.0))
+                        ],
+                      )
+                    ],
+            ),
+          ),
+         bottomNavigationBar: BottomAppBar(
+          child:custom(),
+        ),
+        ),
+    );
+  }
 }
+
+
+class custom extends StatelessWidget {
+  const custom({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        IconButton(onPressed: (){}, icon: const Icon(Icons.phone)),
+        IconButton(onPressed: (){}, icon: const Icon(Icons.message)),
+        IconButton(onPressed: (){}, icon: const Icon(Icons.contact_page)),
+      ],
+    );
+  }
 }
+
